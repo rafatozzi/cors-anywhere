917271bddda41094065e924cd48da9f8fd701bb4
Rafael Tozzi <rafael.tozzi@hotmail.com>
2022-04-14 16:01:12 -0300

    .

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Listen on a specific host via the HOST environment variable
-var host = process.env.HOST || '162.241.41.45';
+var host = process.env.HOST || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable
 var port = process.env.PORT || 3000;
 
@@ -45,6 +45,6 @@ cors_proxy.createServer({
     // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
     xfwd: false,
   },
-}).listen(port, host, function () {
-  console.log('Running CORS Anywhere on ' + host + ':' + port);
+}).listen(port, function () {
+  console.log('Running CORS Anywhere on port ' + port);
 });
