5386e2d84b3d0b9374e5ffc7fec5166ccc7a7fd8
Rafael Tozzi <rafael.tozzi@hotmail.com>
2022-04-14 15:58:32 -0300

    .

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Listen on a specific host via the HOST environment variable
-var host = process.env.HOST || 'cors.dcisuporte.com.br';
+var host = process.env.HOST || '162.241.41.45';
 // Listen on a specific port via the PORT environment variable
 var port = process.env.PORT || 3000;
 
